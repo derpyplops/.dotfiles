c5e0c41732ec332184dc1162fb6f4a68d5723e8f
jon <jonathan.ng1@gmail.com>
2023-04-06 09:45:49 +0000

    ok6

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -2,3 +2,6 @@ ZSH_THEME="powerlevel10k/powerlevel10k"
 plugins=(git)
 source $HOME/.oh-my-zsh/oh-my-zsh.sh
 setopt auto_cd
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
